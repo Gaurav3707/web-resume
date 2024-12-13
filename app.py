acc34e0b1c9ad4a5d5631a78a2d1b40e37f36470
gaurav3707 <gaurav22gautam@gmail.com>
2024-12-13 16:57:29 +0530

    migration to flask completed

--- app.py
+++ app.py
@@ -10,5 +10,22 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
